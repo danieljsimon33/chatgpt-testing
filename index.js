08349f568b30acdb5dc116c84e5f7294fc15f9ea
Daniel Simon <danieljsimon33@gmail.com>
2025-05-14 11:40:07 +0300

    added locally existing files

---
+++ index.js
@@ -0,0 +1,33 @@
+const OpenAI = require('openai');
+require('dotenv').config();
+
+console.log('File running...');
+
+(async () => {
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+    baseURL: 'https://api.openai.com/v1'
+  });
+
+  const stream = await openai.chat.completions.create({
+    model: 'gpt-4o-2024-08-06',
+    messages: [
+      {
+        role: 'system',
+        content: 'Speak like a pirate.'
+      },
+      {
+        role: 'user',
+        content:
+          'How much wood could a woodchuck chuck if a woodchuck could chuck wood?'
+      }
+    ],
+    stream: true
+  });
+
+  for await (const chunk of stream) {
+    // console.log(chunk);
+    console.log(chunk.choices[0]?.delta?.content);
+    // console.log('****************');
+  }
+})();
